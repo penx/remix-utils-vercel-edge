716646c7d9d2e8bca4bc0163561ef03f3cff8c59
Alasdair McLeay <alasdair.mcleay@me.com>
2024-03-19 14:29:01 +0000

    Example usage of ClientOnly on edge

--- app/routes/edge.tsx
+++ app/routes/edge.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@vercel/remix";
+import { ClientOnly } from "remix-utils/client-only";
 
 export const config = { runtime: "edge" };
 
@@ -8,6 +9,7 @@ export default function Edge() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix@Edge</h1>
+      <ClientOnly>{() => <div>Client only</div>}</ClientOnly>
     </div>
   );
 }
